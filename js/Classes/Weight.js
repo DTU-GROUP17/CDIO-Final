e054f020a918786e59fb8790fb6094ff1b4302ba
Ali.Gh <ght.ali@hotmail.com>
2017-06-13 16:20:18 +0200

    Weight problems fixed thx to Oliver :)

--- js/Classes/Weight.js
+++ js/Classes/Weight.js
@@ -1,111 +1,55 @@
-class Weight {
-
-
-
+class Weight extends Model{
     constructor(id, name, uri, created_at, created_by, updated_at, updated_by, deleted_at, deleted_by) {
-        this.id = id;
+        super(id);
         this.name = name;
-        this.uri = uri;
-        this.created_at = moment(created_at);
+        this.weightUri = uri;
+        this.created_at = created_at;
         this.created_by = created_by;
-        this.updated_at = moment(updated_at);
+        this.updated_at = updated_at;
         this.updated_by = updated_by;
-        this.deleted_at = moment(deleted_at);
+        this.deleted_at = deleted_at;
         this.deleted_by = deleted_by;
     }
 
+    static get uri() {
+        return Setting.weightURI;
+    }
 
-    static all() {
-        return new Promise((resolve, reject) => {
-                $.ajax({
-                url: Setting.baseURI+'weights',
-                contentType: "application/json; charset=utf-8",
-                dataType: 'json',
-                'beforeSend': function (request) {
-                    request.setRequestHeader("Authorization", "Bearer " + Cookies.get('token'));
-                },
-            })
-                .done(function(data) {
-
-                    let weights = [];
-
-                    data.forEach(function (weight) {
-
-
-                        weights.push(new Weight(weight.id, weight.name , weight.uri, weight.createdAt, weight.createdBy
-                            , weight.updatedAt, weight.updatedBy, weight.deletedAt, weight.deletedBy));
-                    });
-                    resolve(weights);
-                })
-                .fail(function () {
-                    reject();
-                })
-    });
+    get uri() {
+        return Setting.weightURI;
     }
 
     /**
      *
-     * @returns {{name: string, uri: string}}
+     * @param object
+     * @private
+     * @returns Weight
+     */
+    static _responseToObject(object) {
+        return new Weight(
+            object.id,
+            object.name,
+            object.uri,
+            object.createdAt === null ? null : moment(object.createdAt),
+            object.createdBy === null ? null : object.createdBy.id,
+            object.updatedAt === null ? null : moment(object.updatedAt),
+            object.updatedBy === null ? null : object.updatedBy.id,
+            object.deletedAt === null ? null : moment(object.deletedAt),
+            object.deletedBy === null ? null : object.deletedBy.id
+        );
+    }
+
+    /**
+     *
+     * @returns {{id: int, name: string, uri: string}}
      */
     toArray() {
         return {
-
+            'id' : this.id,
             'name' : this.name,
-            'uri' : this.uri,
-
+            'uri' : this.weightUri,
         }
     }
-
-    /**
-     * @returns {string}
-     */
-    toJson() {
-        return JSON.stringify(this.toArray());
-    }
-
-    /**
-     *
-     * @returns {string}
-     */
-    toString() {
-        return this.toJson();
-    }
-
-    /**
-     * @returns {string}
-     */
-    toStringWithoutId() {
-        let array = this.toArray();
-        delete array.id;
-        return JSON.stringify(array);
-    }
-
-
-    /**
-     *
-     * @return {Promise}
-     */
-    destroy() {
-        return new Promise((resolve, reject) => {
-            $.ajax({
-                url: Setting.weightURI+this.id,
-                type : 'DELETE',
-                contentType: "application/json; charset=utf-8",
-                dataType: 'json',
-                beforeSend : function (request) {
-                    request.setRequestHeader("Authorization", "Bearer " + Cookies.get('token'));
-                },
-            })
-                .done(function(data) {
-                    resolve(data);
-                })
-                .fail(function(message) {
-                    reject(message);
-                })
-        });
-    }
-
-
 
     /**
      *
@@ -123,7 +67,7 @@ class Weight {
                 },
                 data : JSON.stringify({
                     'name' : this.name,
-                     'uri' : this.uri,
+                     'uri' : this.weightUri,
                 })
             })
                 .done(function(data) {
@@ -134,49 +78,6 @@ class Weight {
                 })
         });
     }
-
-    create() {
-        return new Promise((resolve, reject) => {
-            let self = this;
-            $.ajax({
-                url: Setting.weightURI,
-                type : 'POST',
-                contentType: "application/json; charset=utf-8",
-                dataType: 'json',
-                data : this.toStringWithoutId(),
-                beforeSend : function (request) {
-                    request.setRequestHeader("Authorization", "Bearer " + Cookies.get('token'));
-                },
-            })
-                .done(function(data) {
-                    $.ajax({
-                        url: Setting.weightURI,
-                        type : 'GET',
-                        contentType: "application/json; charset=utf-8",
-                        dataType: 'json',
-                        data : 'weightId',
-                        beforeSend : function (request) {
-                            request.setRequestHeader("Authorization", "Bearer " + Cookies.get('token'));
-                        },
-                    }),
-
-                    console.log(data);
-                    resolve(self);
-
-
-                })
-                .fail(function(message) {
-                    reject(message);
-                })
-        });
-    }
-
-
-
-
-
-
-
 
 }
 
