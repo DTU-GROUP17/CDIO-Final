a9f880969dee7328a81b02cf60e5b3afdbeae94a
Ali.Gh <ght.ali@hotmail.com>
2017-06-13 16:37:04 +0200

    Delete that ****

--- vendor/Datavision/ObjectSourceTest.bat
+++
@@ -1,3 +0,0 @@
-cls
-call javac -classpath ../lib/datavision.jar ObjectSourceTest.java
-java -cp .;..lib/liquidlnf.jar;../lib/datavision.jar;../lib/commons-logging.jar;../lib/asm.jar;../lib/bsf.jar;../lib/jruby.jar ObjectSourceTest
